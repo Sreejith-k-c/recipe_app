40d709e78fbc21c8254b3de6d7d4df6749bd3467
AmosPeterAlex <amospeteralex28@gmail.com>
2024-04-16 10:09:36 +0530

    chef verification done in home

--- lib/presentation/homepage/view/homepage.dart
+++ lib/presentation/homepage/view/homepage.dart
@@ -33,9 +33,11 @@ class _HomeScreenState extends State<HomeScreen> {
         leading: Padding(
           padding: const EdgeInsets.only(left: 10),
           child: Consumer<UserProfileController>(builder: (context, controller, _) {
+            var imageUrl = controller.usernameEmailModel.image== null
+                ? "https://th.bing.com/th/id/OIP.y6HMdOJ4LiIUWk7n5ZGlpAHaHa?w=480&h=480&rs=1&pid=ImgDetMain"
+                : controller.usernameEmailModel.image ;
             return CircleAvatar(
-              backgroundImage: NetworkImage(controller.userAvatarModel.avatar ??
-                  "https://t4.ftcdn.net/jpg/00/65/77/27/360_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg"),
+              backgroundImage: NetworkImage(imageUrl),
             );
           }),
         ),
